795e4cd6997cc22438e68bb7d91cfc25139d55c0
bhaveshmhadse <70565774+bhaveshmhadse@users.noreply.github.com>
2021-03-11 15:30:57 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,65 +1,36 @@
-function sleep(time) {
-  return new Promise(resolve => setTimeout(resolve, time));
+function s() {
+  document.getElementById('section3').scrollIntoView();
 }
+let i = 0;
 
-async function profile() {
-  document.getElementById('profile').src = 'profile.jpg';
-  document.getElementById('profile').style = 'visibility:visible;';
-}
-
-async function sayMyName() {
-  let description = ["I'm", 'Bhavesh', ',', 'a', 'developer'];
-  let newName = '';
-  for (let words of description) {
-    if (words === ',') {
-      await sleep(1500);
-    }
-    newName += words + ' ';
-    await sleep(700);
-    document.getElementById('name').innerText = newName;
+function sayMyName() {
+  let intro = [' ', "I'm", ' Bhavesh', ',', ' a', ' developer.'];
+  if (i < intro.length) {
+    document.getElementById('name').innerHTML += intro[i];
+    i++;
+    setTimeout(sayMyName, 700);
   }
 }
-
-async function description() {
-  let description = [
-    'I like programming and doing some interactive things with code.\n',
-    'My Goal is to make good, robust software with clean coding.',
-  ];
-  let newName = '';
-  for (let words of description) {
-    newName += words;
-    await sleep(1000);
-    document.getElementById('description').innerText = newName;
-  }
+function description() {
+  document.getElementById('description').style = 'visibility: visible;';
+}
+function button() {
+  document.getElementById('project').style = 'visibility: visible;';
 }
 
-async function getInTouch() {
-  document.getElementById('info').innerText = 'Get In Touch';
+function goToAbout() {
+  document.getElementById('section3').scrollIntoView();
 }
-
-async function socialMedia() {
-  document.getElementById('1').src = 'instagram.png';
-  document.getElementById('1').style = 'visibility: visible;';
-
-  document.getElementById('2').src = 'github.png';
-  document.getElementById('2').style = 'visibility: visible;';
-
-  document.getElementById('3').src = 'whatsapp.png';
-  document.getElementById('3').style = 'visibility: visible;';
-
-  document.getElementById('4').src = 'facebook.png';
-  document.getElementById('4').style = 'visibility: visible;';
-
-  document.getElementById('5').src = 'twitter.png';
-  document.getElementById('5').style = 'visibility: visible;';
+function goToProjects() {
+  document.getElementById('section2').scrollIntoView();
+}
+function goToHome() {
+  document.getElementById('section1').scrollIntoView();
 }
 
 window.onload = function () {
-  setTimeout(profile, 1000);
-  setTimeout(sayMyName, 2000);
-  setTimeout(description, 7000);
-  setTimeout(getInTouch, 13000);
-  setTimeout(socialMedia, 14300);
+  setTimeout(sayMyName, 0);
+  setTimeout(description, 5000);
+  setTimeout(button, 8000);
+  //   setTimeout(last, 9000);
 };
-
-
